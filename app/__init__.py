e0be279fbe8be15037b3e16ff4486de8737877c2
luciehrbkova <50968340+luciehrbkova@users.noreply.github.com>
2020-11-07 17:25:19 +0000

    login done

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,15 @@ from config import Config
 # config is the name of config.py module & Config is the name of actual class
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+#login Manager
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+# protecting other pages from unlogged users
+login.login_view = 'login'
 
 from app import routes, models
